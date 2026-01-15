dbfd88d23b1e568b74b2eb4eafbdab8095182b22
MonsterLuni <monsterluni@gmail.com>
2026-01-15 16:08:44 +0100

    database now works as intended

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,43 +7,55 @@ export default function SensorScreen() {
   const [worldAcceleration, setWorldAcceleration] = useState({ x: 0, y: 0, z: 0});
   const [accelerationData, setAccelerationData] = useState({ x: 0, y: 0, z: 0, timestamp: 0 });
   const [rotationData, setRotationData] = useState({ alpha: 0, beta: 0, gamma: 0, timestamp: 0 });
-  const [recording, setRecording] = useState(false);
-  const [startTime, setStartTime] = useState<number>(0);
-  const [recordingId, setRecordingId] = useState<number | null>(null);
   const [showData, setShowData] = useState(false);
   const dbRef = useRef<SQLite.SQLiteDatabase | null>(null);
+  const recording = useRef(false);
+  const recordingId = useRef<number | null>(null);
+  const startTime = useRef<number | null>(null);
 
   useEffect(() => {
-    if (recording) {
-      if(dbRef.current === null){
-      throw new Error("Database not initialized");
-      }
-      const startTime = Date.now();
-      setStartTime(startTime);
-      console.log(startTime);
-      const result = dbRef.current.runSync(`INSERT INTO recording (startTime) VALUES (?);`, [startTime]);
-      setRecordingId(result.lastInsertRowId);
-    }
-  }, [recording]);
+    deleteDatabase();
+    initializeDatabase();
 
-  initializeDatabase();
-
-  DeviceMotion.setUpdateInterval(250);
-  DeviceMotion.addListener(async (deviceMotion) => {
+    DeviceMotion.setUpdateInterval(250);
+    DeviceMotion.addListener(async (deviceMotion) => {
     // acceleration in m/s²
     setAccelerationData(deviceMotion.acceleration ?? { x: 0, y: 0, z: 0, timestamp: 0 });
     setRotationData(deviceMotion.rotation);
     await calculateWorldAcceleration();
   });
+  }, []);
+
+  async function toggleRecording(){
+    if (!dbRef.current) throw new Error("DB not initialized");
+
+    recording.current ? stopRecording() : startRecording();
+    recording.current = !recording.current;
+  }
+
+  async function startRecording(){
+      startTime.current = Date.now();
+      const result = dbRef.current!.runSync(`INSERT INTO recording (startTime) VALUES (?);`, [startTime.current]);
+      recordingId.current = result.lastInsertRowId;
+  }
+
+  async function stopRecording(){
+    await dbRef.current!.runAsync(`UPDATE recording SET endTime = ? WHERE id = ?;`,[Date.now(), recordingId.current]);
+  }
 
   async function initializeDatabase() {
     var db = await SQLite.openDatabaseAsync('Motion.db');
     
     await db.execAsync(`
+      PRAGMA foreign_keys = ON;
       CREATE TABLE IF NOT EXISTS recording (id INTEGER PRIMARY KEY AUTOINCREMENT, startTime INTEGER NOT NULL, endTime INTEGER);
-      CREATE TABLE IF NOT EXISTS motion (id INTEGER PRIMARY KEY AUTOINCREMENT,recordingId INTEGER NOT NULL,recordingFK INTEGER PRECISION NOT NULL,x DOUBLE PRECISION NOT NULL,y DOUBLE PRECISION NOT NULL,z DOUBLE PRECISION NOT NULL,duration INTEGER NOT NULL, FOREIGN KEY (recordingId) REFERENCES recording(id));
+      CREATE TABLE IF NOT EXISTS motion (id INTEGER PRIMARY KEY AUTOINCREMENT, recordingFK INTEGER NOT NULL, x REAL NOT NULL, y REAL NOT NULL, z REAL NOT NULL, duration INTEGER NOT NULL, FOREIGN KEY(recordingFK) REFERENCES recording(id));
     `);
     dbRef.current = db;
+  }
+
+  async function deleteDatabase(){
+    await SQLite.deleteDatabaseAsync('Motion.db');
   }
 
   async function calculateWorldAcceleration() {
@@ -56,7 +68,7 @@ export default function SensorScreen() {
     worldAcceleration.y = ((accelerationData.y * cosBeta) * cosAlpha) * cosGamma
     worldAcceleration.z = ((accelerationData.z * cosAlpha) * cosAlpha) * cosBeta
 
-    if(recording){
+    if(recording.current){
       await saveInDatabase();
     }
   }
@@ -65,30 +77,46 @@ export default function SensorScreen() {
     if(dbRef.current === null){
       throw new Error("Database not initialized");
     }
-    if(!startTime){
-      throw new Error("Start time is null");
-    }
-    if(!recordingId){
+    if(recordingId.current === null){
       throw new Error("Recording ID is null");
     }
+    if(startTime.current === null){
+      throw new Error("Start time is null");
+    }
 
-    const timeSinceLastRecord = accelerationData.timestamp - (startTime);
-    await dbRef.current.runAsync(`INSERT INTO motion (recordingId, x, y, z, duration) VALUES (?, ?, ?, ?, ?);`, [recordingId, worldAcceleration.x, worldAcceleration.y, worldAcceleration.z, timeSinceLastRecord]);
+    const timeSinceLastRecord = accelerationData.timestamp - (startTime.current);
+    console.log("INNERHALB DES LOOPS MIT RECORDINGID: " + recordingId.current);
+    try{
+      await dbRef.current.runAsync(`INSERT INTO motion (recordingFK, x, y, z, duration) VALUES (?, ?, ?, ?, ?);`, [recordingId.current, worldAcceleration.x, worldAcceleration.y, worldAcceleration.z, timeSinceLastRecord]);
+    }catch(e){
+      console.error("Error inserting data:", e);
+    }
   }
+
+  type MotionRow = {
+    id: number;
+    recordingFK: number;
+    x: number;
+    y: number;
+    z: number;
+    duration: number;
+  };
 
   async function getRecording(id: number) {
     if(dbRef.current === null){
       throw new Error("Database not initialized");
     }
-    const result = await dbRef.current.getAllAsync(`SELECT id, recordingId, x, y, z, duration FROM motion WHERE recordingId = ? ORDER BY id ASC;`, [id]);
-    console.log(result);
+    const result = await dbRef.current.getAllAsync<MotionRow>(`SELECT id, recordingFK, x, y, z, duration FROM motion WHERE recordingFK = ? ORDER BY id ASC;`, [id]);
+    result.forEach(motionRow => {
+      console.log(`ID: ${motionRow.id}, RecordingFK: ${motionRow.recordingFK}, X: ${motionRow.x}, Y: ${motionRow.y}, Z: ${motionRow.z}, Duration: ${motionRow.duration}`);
+    });
     setShowData(true);
   }
 
   return (
       <View style={styles.container}>
-          <Button onPress={() => getRecording(recordingId ?? 0)} title="Get Last Recording"/>
-          <Button onPress={() => setRecording(!recording)} title={recording ? "Stop Recording" : "Start Recording"}/>
+          <Button onPress={() => getRecording(recordingId.current ?? 0)} title="Get Last Recording"/>
+          <Button onPress={() => toggleRecording()} title={recording.current ? "Stop Recording" : "Start Recording"}/>
           <Text>Acceleration:</Text>
           <Text>x: {accelerationData.x}</Text>
           <Text>y: {accelerationData.y}</Text>
